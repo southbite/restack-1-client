1ae1724eeadb37ccdeac37ebedac9b082454bc08
Southbite <southbite@gmail.com>
2013-09-20 15:16:16 +0200

    extended script to include updates

--- test/client_test_script.js
+++ test/client_test_script.js
@@ -19,17 +19,40 @@ restack_client.initialize({api_url:'http://localhost:7777'}, function(e){
             //POST:function(area, type, data, headers, done){
             restack_client.POST('data', 'User', user, null, function(response){
             	
-            	console.log(response);
+            	 if (response.error == null)
+            	 {
+	            	console.log(response.data);
+	            	
+	            	  console.log('getting users');
+	                  //GET:function(area, type, criteria, headers, done){
+	                  restack_client.GET('data', 'User', {emailaddress: 'test@example.com'}, null, function(response){
+	                  	
+	                	 if (response.error == null)
+	                	 {
+	                		 console.log("Get worked");
+	                		 //PUT:function(area, type, criteria, data, headers, done){
+	                		 restack_client.PUT('data', 'User', {emailaddress: 'test@example.com'}, {newproperty:'updated now', firstname:'updated firstname'}, null, function(response){
+	                			
+	                			console.log('update response');
+	                			console.log(response.data);
+	                			 
+	                			if (response.error == null)
+	                			{
+	                				 restack_client.GET('data', 'User', {emailaddress: 'test@example.com'}, {'Include-Deleted':true}, function(response){
+	                					 console.log('get after update response');
+	     	                			console.log(response.data);
+	                				 }.bind(this));
+	                			}
+	                			 
+	                		 }.bind(this));
+	                		 
+	                	 }
+	                	
+	                  	
+	                  }.bind(this));
+            	 }
             	
-            	  console.log('getting users');
-                  //GET:function(area, type, criteria, headers, done){
-                  restack_client.GET('data', 'User', {emailaddress: 'test@example.com'}, null, function(response){
-                  	
-                  	console.log(response);
-                  	
-                  });
-            	
-            });
+            }.bind(this));
             
           
 	}
